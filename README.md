babd8c6134c09a1b2fdaf20ddb1d671e5070bcdf
Elichai Turkel <elichai.turkel@gmail.com>
2019-03-04 01:05:45 +0200

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Bits128
 Iterating over 128 array of bytes
+
+
+`bits128` provides a struct that let's you use 128 bits while taking only 128 bits in memory. <br>
+if you would use something like `[bool; 128]` it would take 128*8 bits in memory because every bool takes 1 bytes(8bits) <br>
+In the future I'll implement an Iterator over the bits so you can iterate over them easily. <br>
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![no_std]
 
 //! # bits128
-//! `bits128` provides a struct that let's you use 128 bits while taking only 128 bits in memory.
-//! if you would use something like `[bool; 128]` it would take 128*8 bits in memory because every bool takes 1 bytes(8bits)
-//! In the future I'll implement an Iterator over the bits so you can iterate over them easily.
+//! `bits128` provides a struct that let's you use 128 bits while taking only 128 bits in memory. <br>
+//! if you would use something like `[bool; 128]` it would take 128*8 bits in memory because every bool takes 1 bytes(8bits) <br>
+//! In the future I'll implement an Iterator over the bits so you can iterate over them easily. <br>
 
 
 use core::{fmt, ops};
